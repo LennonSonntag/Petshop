5b8aae0df0599433731e0d67f4ac5a1245595e4d
darlanfassbinder123-ui <darlanfassbinder123@gmail.com>
2026-06-28 17:06:26 -0300

    Update Tosa.java

--- src/Tosa.java
+++ src/Tosa.java
@@ -3,6 +3,38 @@ public class Tosa extends Servico{
     public Tosa(Animal animal) {
         super(animal);
     }
+    
+    //Darlan: Fiz uma alteração pois gato não tem diferença de tamanho. Se formos utilizar, apagamos o outro.
+    /*
+     @Override
+    public double precificacao() {
+
+        // Regra para gato:
+        // A tosa de gato possui valor único.
+        if (animal instanceof Gato) {
+            return 70;
+        }
+
+        // Regra para cachorro:
+        // O valor da tosa muda conforme o porte.
+        if (animal instanceof Cachorro) {
+            switch (animal.getPorte()) {
+                case 'P':
+                    return 50;
+
+                case 'M':
+                    return 60;
+
+                case 'G':
+                    return 80;
+
+                default:
+                    return 0;
+            }
+        }
+
+        return 0;
+        */
 
     @Override
     public double precificacao() {
