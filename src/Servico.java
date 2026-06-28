2d3f2bc9eb6c9c26cbe5bfdce8a2ce82c3266582
darlanfassbinder123-ui <darlanfassbinder123@gmail.com>
2026-06-28 15:42:15 -0300

    Fix constructor to assign animal parameter
    Added assignment of animal parameter to the class variable.

--- src/Servico.java
+++ src/Servico.java
@@ -5,6 +5,8 @@ public abstract class Servico {
     private String descricao;
 
     public Servico(Animal animal) {
+        //Darlan: Acho que precisa da linha abaixo:
+        this.animal = animal;
     }
 
     public void String(String descricao){
