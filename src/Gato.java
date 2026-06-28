b95482fadaa1184872f679d8bfd184b81bef85ff
darlanfassbinder123-ui <darlanfassbinder123@gmail.com>
2026-06-28 17:16:00 -0300

    Refactor Gato class by removing unused code
    Removed the commented-out constructor and toString method for Gato.

--- src/Gato.java
+++ src/Gato.java
@@ -1,6 +1,19 @@
 // Herda de animal.
 public class Gato extends Animal{
 
+    //Darlan: Aqui apaguei o porte do gato.
+
+    /*
+    public Gato(String nome){
+        super(nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato - Nome: " + getNome();
+    }
+    */
+
     public Gato(String nome, char porte){
         super(nome, porte);
     }
